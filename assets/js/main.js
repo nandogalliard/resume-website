707fd433738c850c91c14fd42951306f853333d8
Nando <gnando@student.ethz.ch>
2022-09-07 23:16:58 +0200

    Added German language and CV

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,35 @@ jQuery(document).ready(function($) {
 
 
 });
+
+// Checking if switch exists
+if (document.querySelector(".switch label")) {
+	const langSwitcher = document.querySelector(".switch label");
+	langSwitcher.addEventListener("click", () => {
+	  switchLang();
+	});
+  
+	// Switching language
+	function switchLang() {
+	  var i18n = {};
+	  // Selecting all language specific elements
+	  i18n.de = document.getElementsByClassName("contentdeutsch");
+	  i18n.en = document.getElementsByClassName("contentenglish");
+	  // Determining current language
+	  i18n.lang =
+		window.getComputedStyle(i18n.en[0]).display != "none" ? "en" : "de";
+  
+	  // Hiding current language and turning on the other one
+	  if (i18n.lang == "en") {
+		for (var i = 0; i < i18n.de.length; i++) {
+		  i18n.de[i].style.display = "block";
+		  i18n.en[i].style.display = "none";
+		}
+	  } else {
+		for (var i = 0; i < i18n.de.length; i++) {
+		  i18n.de[i].style.display = "none";
+		  i18n.en[i].style.display = "block";
+		}
+	  }
+	}
+  }
